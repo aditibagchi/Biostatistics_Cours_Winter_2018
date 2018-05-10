f42cc7846f749d8ff599c409b7bfd12d384b102d
aditibagchi <aditi.bagchi@vai.org>
2018-05-10 09:26:31 -0400

    First commit of the test 5102018

---
+++ Starting the test .R
@@ -0,0 +1,7 @@
+##Rcmdr:: was instaled and used for the test 
+
+library("Rcmdr", lib.loc="/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
+
+##Imported the data set and had alook of the CSV file
+
+View(Question_1)
